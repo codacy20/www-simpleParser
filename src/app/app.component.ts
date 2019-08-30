8871d477ea89ec14d15ed8d07830c995ac10e9ae
codacy20 <kmuc72@gmail.com>
2019-08-30 16:39:21 +0200

    parsing the loaded file

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,13 +9,38 @@ export class AppComponent {
     title = 'www-simpleParser';
     fileToUpload: File = null;
     csvContent: string;
+    JSONData: any;
 
     constructor(private fileUploadService: UploadService) {}
+
+    csvJSON(csvText) {
+        const lines = csvText.split('\n');
+
+        const result = [];
+
+        const headers = lines[0].split(',');
+        console.log(headers);
+        for (let i = 1; i < lines.length - 1; i++) {
+            const obj = {};
+            const currentline = lines[i].split(',');
+
+            for (let j = 0; j < headers.length; j++) {
+                obj[headers[j]] = currentline[j];
+            }
+
+            result.push(obj);
+        }
+
+        // return result; //JavaScript object
+        console.log(JSON.stringify(result)); // JSON
+        this.JSONData = JSON.stringify(result);
+    }
 
     onFileLoad(fileLoadedEvent) {
         const textFromFileLoaded = fileLoadedEvent.target.result;
         this.csvContent = textFromFileLoaded;
-        console.log(this.csvContent);
+        // console.log(this.csvContent);
+        this.csvJSON(this.csvContent);
     }
 
     onFileSelect(input: HTMLInputElement) {
@@ -23,7 +48,9 @@ export class AppComponent {
         if (files && files.length) {
             const fileToRead = files[0];
             const fileReader = new FileReader();
-            fileReader.onload = this.onFileLoad;
+            // fileReader.onload = this.onFileLoad;
+            fileReader.onload = fileLoadedEvent =>
+                this.onFileLoad(fileLoadedEvent);
             fileReader.readAsText(fileToRead, 'UTF-8');
         }
     }
